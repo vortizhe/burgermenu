271e3c3aed35f6793b68e59c684f797bf445409f
Victor Ortiz <kespers@gmail.com>
2016-02-12 19:08:03 +0100

    Demo fixes wip

--- js/ready.js
+++ js/ready.js
@@ -1,18 +1,11 @@
-$(function(){
+$(function() {
+
   // Demo 1 & 4
   $('#menu-example').burgermenu();
 
   // Demo 2
-  $('#menu-example-right').burgermenu({
-    position: "right"
+  $('#right-example').burgermenu({
+    position: 'right'
   });
 
-  // Demo 3
-  $('#menu-example1').burgermenu({
-    toggle_selector: '#menu-toggle1'
-  });
-  $('#menu-example2').burgermenu({
-    position: 'right',
-    toggle_selector: '#menu-toggle2'
-  });
 });
